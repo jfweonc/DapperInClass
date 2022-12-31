f3661a3420551203c9c7b2d88c700e153c6bef2a
Guolong Li <Guolong.LiRealtor@gmail.com>
2022-12-30 22:42:02 -0600

    dapperInClass

---
+++ DapperProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperInClass
+{
+    public class DapperProductRepository: IProductRepository
+    {        
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+        public void CreateProduct(string newProduct,double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO products (Name) VALUES (@productName);",
+        new { productName = newProduct });
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperInClass;
+using Dapper;
+
+Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+conn.Execute("INSERT INTO DEPARTMENTS (Name) VALUES ('DellLaptop');"); 
+/*
+var repo = new DapperDepartmentRepository(conn);
+
+Console.WriteLine("Type a new Department name"); 
+
+var newDepartment = Console.ReadLine();
+
+repo.InsertDepartment(newDepartment);
+
+var departments = repo.GetAllDepartments();
+
+foreach(var dept in departments)
+{
+    Console.WriteLine(dept.Name);
+}
+Console.WriteLine("Type a new Product name, price, and category ID:");
+
+var repo1 = new DapperProductRepository(conn);
+
+var newProduct = Console.ReadLine();
+
+repo1.CreateProduct(newProduct,0,0);
+
+repo1.GetAllProducts(); 
+*/
